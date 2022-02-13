97b580ce8a5227a1197637655c3a993bbaa686a5
Grigoriev Valerii <g.valera15102003@gmail.com>
2022-02-14 01:55:15 +0500

    Deploy

---
+++ src/redux/actions/pizzas.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const setLoaded = (payload) => ({
+    type: 'SET_LOADER',
+    payload: payload,
+})
+
+export const fetchPizzas = (name, category) => (dispatch) => {
+    console.log(name)
+    console.log(category)
+    dispatch(setLoaded(false))
+    axios.get(`http://localhost:3001/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${name}&_order=asc`).then(resp => {
+        console.log()
+        dispatch(setPizzas(resp.data));
+    })
+}
+
+export const setPizzas = (items) => ({
+    type: 'SET_PIZZAS',
+    payload : items
+});
